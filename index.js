0a87d34339f55b62d3d77f929f19467e5f809a24
+ <+>
2022-12-31 19:28:22 +0300

    added javascript constants

---
+++ index.js
@@ -0,0 +1,10 @@
+// JavaScript
+// Constants
+const searchForm = document.querySelector('#search-form');
+const searchTermInput = document.querySelector('#search-term');
+const booksContainer = document.querySelector('#books-container');
+const bookModal = document.querySelector('#book-modal');
+const ratingModal = document.querySelector('#rating-modal');
+const ratingForm = document.querySelector('#rating-form');
+const modalTitle = document.querySelector('#modal-title');
+const modalBody = document.querySelector('#modal-body');
